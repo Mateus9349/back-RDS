e740abc60d14555a75a729ae278679e6d5cc650e
Mateus9349 <mateusvpdl@outlook.com>
2023-07-14 10:35:12 -0400

    liberando o acesso por https

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const https = require('https');
+
 const express = require('express');
 const routes = require('./routes')
 
@@ -7,6 +10,14 @@ const port = 8000;
 
 routes(app)
 
-app.listen(port, ()=> console.log(`O servidor esta rodando na porta ${port}`))
+app.listen(port, ()=> console.log(`O servidor esta rodando na porta ${port}`));
+
+https.createServer({
+    cert: fs.readFileSync('api/SSL/certificate.crt'),
+    key: fs.readFileSync('api/SSL/private.key'),
+    ca: fs.readFileSync('api/SSL/ca_bundle.crt')
+}, app).listen(8001, ()=> console.log('Rodando em https'));
+
+
 
 module.exports = app
